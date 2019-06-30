9d224e9fe9a97cd6d84868c5aa94d3a05c672a90
Subin <subin@apache.org>
2019-06-30 17:45:01 -0400

    Add manylinux patch

--- build_tools/build_bazel.sh
+++ build_tools/build_bazel.sh
@@ -6,10 +6,18 @@
 gcc -v
 python -V
 FULL_JAVA_HOME=$(readlink -f $JAVA_HOME)
-echo $FULL_JAVA_HOME
+echo "FULL_JAVA_HOME="$FULL_JAVA_HOME
 export JAVA_HOME=$FULL_JAVA_HOME
 cd /opt/app-root/
-./compile.sh
+echo "BAZEL_VERSION="$BAZEL_VERSION
+if [ "$BAZEL_VERSION" = "0.24.1" ] ; then 
+    # mkdir -p /opt/app-root/output/
+    # cd /opt/app-root/output/
+    # wget https://github.com/sub-mod/bazel-builds/releases/download/0.24.1/bazel-0.24.1
+    ls -l /opt/app-root/output/
+else
+    ./compile.sh
+fi
 # put /opt/app-root/output/bazel in path
 export PATH=/opt/app-root/output/:$PATH
 # cp /opt/app-root/output/bazel /usr/local/bin/
