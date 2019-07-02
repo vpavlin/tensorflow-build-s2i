09947223363c0aa9edafcd90c6c43acf496f661e
Subin <subin@apache.org>
2019-07-01 22:32:32 -0400

    Add devtoolset-8

--- build_tools/setup_devtoolset.sh
+++ build_tools/setup_devtoolset.sh
@@ -30,6 +30,24 @@ if [ "$DEV_TOOLSET_VERSION" = "7" ] ; then
 		ln -s /opt/rh/devtoolset-$DEV_TOOLSET_VERSION/root/usr/bin/ld /usr/bin/ld;
 	fi
 fi
+if [ "$DEV_TOOLSET_VERSION" = "8" ] ; then 
+	if [[ "$OS_VERSION" = "rhel6" ]] || [[ "$OS_VERSION" = "centos6" ]] ; then
+		echo "Found environment $OS_VERSION-$PYTHON_VERSION"  &&
+		export MANPATH=/opt/rh/devtoolset-8/root/usr/share/man:$MANPATH &&
+		export INFOPATH=/opt/rh/devtoolset-8/root/usr/share/info:$INFOPATH &&
+		export PCP_DIR=/opt/rh/devtoolset-8/root:$INFOPATH &&
+		export LD_LIBRARY_PATH=/opt/rh/devtoolset-8/root/usr/lib64:/opt/rh/devtoolset-8/root/usr/lib:/opt/rh/devtoolset-8/root/usr/lib64/dyninst:/opt/rh/devtoolset-8/root/usr/lib/dyninst:/opt/rh/devtoolset-8/root/usr/lib64:/opt/rh/devtoolset-8/root/usr/lib:$LD_LIBRARY_PATH &&
+		export PATH=/opt/rh/devtoolset-8/root/usr/bin:$PATH &&
+		export PYTHONPATH=/opt/rh/devtoolset-8/root/usr/lib64/python2.6/site-packages:/opt/rh/devtoolset-8/root/usr/lib/python2.6/site-packages:$PYTHONPATH &&
+		export PERL5LIB=/opt/rh/devtoolset-8/root//usr/lib64/perl5/vendor_perl:/opt/rh/devtoolset-8/root/usr/lib/perl5:/opt/rh/devtoolset-8/root//usr/share/perl5/vendor_perl &&
+		rm -fr /usr/bin/gcc &&
+		rm -fr /usr/bin/g++ &&
+		rm -fr /usr/bin/ld &&
+		ln -s /opt/rh/devtoolset-$DEV_TOOLSET_VERSION/root/usr/bin/gcc /usr/bin/gcc &&
+		ln -s /opt/rh/devtoolset-$DEV_TOOLSET_VERSION/root/usr/bin/g++ /usr/bin/g++ &&
+		ln -s /opt/rh/devtoolset-$DEV_TOOLSET_VERSION/root/usr/bin/ld /usr/bin/ld;
+	fi
+fi
 
 echo "LD_LIBRARY_PATH ="$LD_LIBRARY_PATH;
 echo "MANPATH ="$MANPATH;
